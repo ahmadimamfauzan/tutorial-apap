638377f1d90eebe6663f56ed112114366c03d406
ahmadimamfauzan <ahmadimamfauzan@gmail.com>
2021-10-04 20:24:56 +0700

    add demo-tutorial-4

--- 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BaseController.java
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BaseController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalTime;
+
 @Controller
 public class BaseController {
 
     @GetMapping("/")
     private String home(Model model) {
+        LocalTime current = LocalTime.now();
+        model.addAttribute("waktuAkses", current);
         model.addAttribute("activePage", "home");
         return "home";
     }
